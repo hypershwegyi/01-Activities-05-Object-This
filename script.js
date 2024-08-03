a7f36b66b04844b41ed0aaa23aba5b6fbbabf2ea
hypershwegyi <shweyeetheint690@email.com>
2024-08-03 17:32:58 +0630

    sixth

---
+++ script.js
@@ -0,0 +1,31 @@
+// TODO: What is `this` referring to here?
+console.log(this);
+
+// TODO: What is `this` referring to here?
+function helloThis() {
+  console.log('Inside this function, this is ' + this);
+}
+
+// TODO: What is `this` referring to here? What will be logged in the console?
+var child = {
+  age: 10,
+  ageTenYears: function() {
+    console.log(this.age + 10);
+  }
+};
+
+// TODO: What is `this` referring to here? What will be logged in the console?
+var investor = {
+  name: 'Child',
+  investment: {
+    initialInvestment: 5000,
+    investmentGrowth: function() {
+      console.log(this.initialInvestment * 1.15);
+    }
+  }
+};
+
+// TODO: Call the `helloThis` function and the object methods to check results in the console
+helloThis();
+child.ageTenYears();
+investor.investment.investmentGrowth();
